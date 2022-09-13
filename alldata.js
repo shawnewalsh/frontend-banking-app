f0f7855166092fb0262d9f21e3af2181a3f17e1f
shawnewalsh <shawn.walsh@gmail.com>
2022-09-13 16:19:58 -0300

    Create Profile and Data pages finished

--- alldata.js
+++ alldata.js
@@ -1,9 +1,55 @@
+
 function AllData(){
+
+  const [show, setShow]         = React.useState(false);
+
   const ctx = React.useContext(UserContext);
+  console.log(ctx.users);
+
+  
+
   return (
+    <Card 
+    style={{maxWidth: "18rem"}}
+    bgcolor="secondary"
+    header="User Table Data"
+    title = "User Data"
+    
+    body = { ctx.users ?  (
+      <>
+      <table className="table">
+<thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <th scope="col">Password</th>
+      <th scope="col">Balance</th>
+    </tr>
+    { ctx.users.map((user, i) => (
+      <tr key={i}>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td>{user.password}</td>
+        <td>{user.balance}</td>
+      </tr>
+    ))}
+</thead>
+</table>
+     </>
+
+    ):(
     <>
-    <h5>All Data in Store</h5>
-    {JSON.stringify(ctx)}<br/>
+     <h5>There are no users yet</h5>
     </>
-  );
-}
+    )
+   
+    }
+    
+
+    />
+  )
+    }
+    
+
+  
+
--- createaccount.js
+++ createaccount.js
@@ -3,23 +3,51 @@ function CreateAccount(){
   const [status, setStatus]     = React.useState('');
   const [name, setName]         = React.useState('');
   const [email, setEmail]       = React.useState('');
+  const [validemail, setValidemail]       = React.useState('false');
   const [password, setPassword] = React.useState('');
+  const [isButtonDisabled, setisButtonDisabled] =    React.useState('true');
+
   const ctx = React.useContext(UserContext);  
 
   function validate(field, label){
       if (!field) {
-        setStatus('Error: ' + label);
+        setStatus('Error - Please enter a value for : ' + label);
         setTimeout(() => setStatus(''),3000);
+        console.log(`the field is ${field}`);
         return false;
       }
+      if (label = 'password' && field.length < 8 ){
+        setStatus('Error - Password must be at least 8 characters' + label);
+        return false;
+      }
+      console.log(`the field is ${field} and the datatype of the field is ${typeof(field)}`);
+      setStatus('');
+      setisButtonDisabled(false);
       return true;
+  }
+
+  // console.log(`Status is ' ${status}`)
+  function validateEmail(email) {
+    const emlRgx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if (email.match(emlRgx)){
+      setValidemail(true);
+    }
+    return email.match(emlRgx);
   }
 
   function handleCreate(){
     console.log(name,email,password);
     if (!validate(name,     'name'))     return;
-    if (!validate(email,    'email'))    return;
+    if (!validate(email,    'email')) {
+      return;
+    }   
     if (!validate(password, 'password')) return;
+    console.log(' Email validate is ' + validateEmail(email));
+    if (! validateEmail(email)) {
+      setStatus('Email address is invalid');
+      return;
+    } 
+    setValidemail(true);
     ctx.users.push({name,email,password,balance:100});
     setShow(false);
   }    
@@ -44,7 +72,7 @@ function CreateAccount(){
               <input type="input" className="form-control" id="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.currentTarget.value)}/><br/>
               Password<br/>
               <input type="password" className="form-control" id="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.currentTarget.value)}/><br/>
-              <button type="submit" className="btn btn-light" onClick={handleCreate}>Create Account</button>
+              <button type="submit" className="btn btn-light" onClick={handleCreate} disabled={!(name && email && password)}>Create Account</button>
               </>
             ):(
               <>
@@ -55,3 +83,4 @@ function CreateAccount(){
     />
   )
 }
+
