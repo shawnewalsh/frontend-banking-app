0dde6bc7ddad5f4a567640bc4fc0695c238745db
shawnewalsh <shawn.walsh@gmail.com>
2022-09-14 19:50:26 -0500

    finished withdrawal

--- deposit.js
+++ deposit.js
@@ -2,11 +2,31 @@ function Deposit(){
   const ctx = React.useContext(UserContext);
   // console.log(`context username is ${ctx.userName}`);
   
-  const [deposit, setDeposit]   = React.useState('0');
-  const [balance, setBalance ] = React.useState(100);
-  //const [password, setPassword]   = React.useState('');
+  const [deposit, setDeposit]   = React.useState('');
+  const [balance, setBalance ] = React.useState(ctx.balance);
+  const [status, setStatus]     = React.useState('');
   
+  console.log('ctx balance data type is ' + typeof(ctx.balance));
+
   function handleDeposit() {
+    setStatus('');
+    let pattern = /^\d+\.?\d*$/;
+
+
+    
+    if (parseInt(deposit) < 0 ){
+      setStatus('Please enter positive numbers only');
+      alert('Please enter positive numbers only');
+      return;
+    } 
+
+    if (!pattern.test(deposit)) {
+      setStatus('Please enter numbers only');
+      alert('Please enter numbers only');
+      return;
+    }
+
+
     console.log('ctx balance data type is ' + typeof(ctx.balance));
     console.log('deposit data type is ' + typeof(deposit));
     ctx.balance += parseInt(deposit);
@@ -23,12 +43,12 @@ function Deposit(){
     <Card
       bgcolor="primary"
       header="Deposit Funds"
-
+      status={status}
       body={ctx.loggedin ? (  
         <>
         Amount<br/>
         <input type="input" className="form-control" id="deposit" placeholder="Enter your deposit amount" value={deposit} onChange={e => setDeposit(e.currentTarget.value)} /><br/>
-        <button type="submit" className="btn btn-dark" onClick={handleDeposit} >Confirm Deposit</button>
+        <button type="submit" className="btn btn-dark" onClick={handleDeposit} disabled={!deposit}  >Confirm Deposit</button>
         <br/><br/>
         Current Balance<br/>
         <input type="text" id="balance" name="balance" value={balance} readOnly></input><br/>
--- withdraw.js
+++ withdraw.js
@@ -1,5 +1,67 @@
 function Withdraw(){
+  const ctx = React.useContext(UserContext);
+
+  const [ withdrawal,setWithdrawal ] = React.useState('');
+  const [ balance, setBalance ] = React.useState(ctx.balance);
+  const [status, setStatus]     = React.useState('');
+  console.log('ctx balance data type is ' + typeof(ctx.balance));
+  function handleWithdrawal() {
+    setStatus('');
+    let pattern = /^\d+\.?\d*$/;
+
+
+    
+    if (parseInt(withdrawal) < 0 ){
+      setStatus('Please enter positive numbers only');
+      alert('Please enter positive numbers only');
+      return;
+    } 
+
+    if (!pattern.test(withdrawal)) {
+      setStatus('Please enter numbers only');
+      alert('Please enter numbers only');
+      return;
+    }
+
+
+    if (parseInt(withdrawal) > ctx.balance ){
+      setStatus('transaction failed: You do not have enough funds to make that withdrawal');
+      alert('transaction failed');
+      return;
+
+    }
+    ctx.balance -= parseInt(withdrawal);
+    setBalance(ctx.balance);
+    document.getElementById('balance').innerHTML = balance;    
+    console.log(`balance is now ${ctx.balance}`);
+  }
+
   return (
-    <h1>Withdraw</h1>
+    <Card
+    bgcolor="secondary"
+    header="Withdraw Funds"
+    status={status}
+    body={ctx.loggedin ? (  
+      <>
+      Amount<br/>
+      <input type="input" className="form-control" id="withdraw" placeholder="Enter your withdrawal amount" value={withdrawal} 
+        onChange={e => setWithdrawal(e.currentTarget.value)} /><br/>
+      <button type="submit" className="btn btn-light" onClick={handleWithdrawal} disabled={!withdrawal} >Confirm withdrawal</button>
+      <br/><br/>
+      Current Balance<br/>
+      <input type="text" id="balance" name="balance" value={balance} readOnly></input><br/>
+      
+      </>
+    ):(
+      <>
+      <h5>Please login first to manage withdrawals</h5>
+      </>
+
+    )}
+      
+  
+
+  />
+
   )
 }
